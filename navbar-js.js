9f3e8b6da2ca8e5da84d723bfa67b69d10e44cf0
Chris Gratigny <chrisg@ibethel.org>
2013-03-21 21:41:26 -0700

    fix link

--- navbar-js.js
+++ navbar-js.js
@@ -54,7 +54,7 @@
     AhoNavigation.prototype.sponsors = function() {
       this.append("<ul id='sponsors-nav'>");
       this.append("<li><a href='http://my.abundantharvestorganics.com/public/why-organic/'><img src='https://nav.abundantharvestorganics.com/logo-usda.png' /></a></li>");
-      this.append("<li><a href='http://my.abundantharvestorganics.com/public/why-organic/' /></a></li>");
+      this.append("<li><a href='http://my.abundantharvestorganics.com/public/why-organic/'><img src='https://nav.abundantharvestorganics.com/logo-ccof.png' /></a></li>");
       this.append("<li><a href='http://my.abundantharvestorganics.com/public/why-organic/'>Non-<br />GMO</a></li>");
       return this.append("</ul>");
     };
